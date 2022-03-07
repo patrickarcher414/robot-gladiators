9d832746f8dfe117e38a7db58728459300bc230b
patrickarcher414 <patrickarcher414@gmail.com>
2022-03-07 15:24:54 -0600

    added basic functions to game.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,10 @@
-window.alert("This is an alert! JavaScript is running!")
+var playerName = window.prompt("what is your robot's name");
+console.log(playerName);
+console.log("this is a string, good for leaving yourself a message");
+console.log(30*16);
+console.log("our robot's name is " + playerName);
+function fight() {
+    window.alert("the fight has begun!");
+}
+// fight();
+
