86eecb5de51e7a54f9f6c9b1b616fd5c662903ce
patrickarcher414 <patrickarcher414@gmail.com>
2022-03-07 21:31:29 -0600

    approaching mvp

--- assets/js/game.js
+++ assets/js/game.js
@@ -14,9 +14,9 @@ var enemyAttack = 12;
 
 // fight function
 var fight = function(enemyName) {
-  // Alert players that they are starting the round
-  window.alert("Welcome to Robot Gladiators!");
-
+    // repeat and execute as long as the enemy-robot is alive 
+  while(enemyHealth > 0) {
+ 
   // ask player if they'd like to fight or run
   var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
 
@@ -66,9 +66,12 @@ var fight = function(enemyName) {
   } else {
     window.alert("You need to pick a valid option. Try again!");
   }
+  }
 };
 
 // run fight function to start game
 for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
-}
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth= 50;
+    fight(pickedEnemyName);
+};
